baafd2e704bf14eaadb7d616e33f4c062c81ee83
Jihan <jihanahmed41@gmail.com>
2024-02-06 03:44:56 -0800

    updating base model

--- models/base_model.py
+++ models/base_model.py
@@ -12,14 +12,20 @@ class BaseModel:
     BaseModel that defines all common attributes/methods for other classes
     '''
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''
-        the initialization of attributes
+        the initialization method of attributes
         '''
-
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if not kwargs:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
 
     def __str__(self):
         '''
@@ -30,18 +36,20 @@ class BaseModel:
 
     def save(self):
         '''
-        updates the public instance attribute updated_at with the current datetime
+        updates the public instance attribute updated_at
+        with the current datetime
         '''
 
         self.updated_at = datetime.now()
 
     def to_dict(self):
         '''
-        returns a dictionary containing all keys/values of __dict__ of the instance
+        returns a dictionary containing all keys/values of
+        __dict__ of the instance
         '''
 
-        self.__dict__["__class__"] = self.__class__.__name__
-        self.created_at = self.created_at.isoformat()
-        self.updated_at = self.updated_at.isoformat()
-        return self.__dict__
-
+        instance_dict = self.__dict__.copy()
+        instance_dict["__class__"] = self.__class__.__name__
+        instance_dict["created_at"] = self.created_at.isoformat()
+        instance_dict["updated_at"] = self.updated_at.isoformat()
+        return instance_dict
