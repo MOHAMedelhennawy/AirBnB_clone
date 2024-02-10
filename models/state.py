cb5de35c8c9f11d0a3f72d947654eb5ad9a9e505
Jihan <jihanahmed41@gmail.com>
2024-02-10 13:19:21 -0800

    solving state file

--- models/state.py
+++ models/state.py
@@ -8,7 +8,9 @@ from models.base_model import BaseModel
 class State(BaseModel):
     """
     A subclass of BaseModel class
+
     Public class attribute:
-        name: (str)
+    name (str): state name
     """
+
     name = ""
